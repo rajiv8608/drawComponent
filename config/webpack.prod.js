1ad72e840c306587ae16f968b3964d637644b212
Bhargav Krishna <bhargav.krishna@outlook.com>
2017-02-03 16:41:48 -0800

    adding prod build config

---
+++ config/webpack.prod.js
@@ -0,0 +1,36 @@
+let webpack = require('webpack');
+let webpackMerge = require('webpack-merge');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let commonConfig = require('./webpack.common.js');
+let path = require('path');
+
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+
+module.exports = webpackMerge(commonConfig, {
+    devtool: 'source-map',
+
+    output: {
+        path: path.resolve('dist'),
+        filename: '[name].js',
+        chunkFilename: '[id].chunk.js'
+    },
+
+    htmlLoader: {
+        minimize: false // workaround for ng2
+    },
+
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: {
+                keep_fnames: true
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new ExtractTextPlugin('[name].css')
+    ]
+});
